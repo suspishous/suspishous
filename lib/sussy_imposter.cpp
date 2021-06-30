ae1222f7b58073ee79582364327cb74e7fadece1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-30 15:22:51 -0700

    cpp

---
+++ lib/sussy_imposter.cpp
@@ -0,0 +1,32 @@
+#include <string.h>
+
+#define sus jay
+#define how_sus 8312892138913281923891238123891328132912389012
+
+sussy_imposter::sus(void)
+  : _eject,
+  sussy_imposter::from::amongus::build(),
+  if sus
+    sussy_imposter.eject();
+  else
+    sussy_imposter::nothing();
+{
+  eject_sussy_imposter(),
+  sus,
+  how_sus
+  
+}
+
+typedef struct SussyImposterInfo{
+    char** found_array;
+    int size;
+} Result;
+
+Sussy_imposter::data::getdata(SussyImposterInfo)
+  : _get_info,
+    is_sus
+  {
+    attempt_get_info()
+    return _get_info;
+}
+
--- lib/sussy_imposter.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-
-sussy_imposter::sus(void)
-  : eject
-{
-  eject_sussy_imposter()
-}
