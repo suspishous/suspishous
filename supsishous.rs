ac22403d608ad063fbfc6403928e806c19f8cc59
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-30 12:54:40 -0700

    Update supsishous.rs

--- supsishous.rs
+++ supsishous.rs
@@ -1,3 +1,14 @@
+struct Sus {
+    is_sus: bool,
+    how_sus: u128
+}
+    
+
 fn main() {
     println!("so suspishous");
+    let sussy: Sus = Sus {
+    is_sus: true,
+    how_sus: u128::MAX,
+  };
+    println!("Is sus: {}\nHow sus: {}", sussy.is_sus, sussy.how_sus);
 }
