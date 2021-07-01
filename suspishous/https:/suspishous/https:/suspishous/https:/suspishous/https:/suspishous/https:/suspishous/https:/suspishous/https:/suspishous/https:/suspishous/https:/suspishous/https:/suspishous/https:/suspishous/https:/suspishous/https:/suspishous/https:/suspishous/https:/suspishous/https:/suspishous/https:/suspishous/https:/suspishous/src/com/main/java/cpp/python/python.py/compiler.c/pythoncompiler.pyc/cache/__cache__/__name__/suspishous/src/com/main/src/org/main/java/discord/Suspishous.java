122e673b599a232be5a15b3f6b9f46b9630c4a1f
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-07-01 14:57:25 -0400

    Create Suspishous.java

---
+++ suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/https:/suspishous/src/com/main/java/cpp/python/python.py/compiler.c/pythoncompiler.pyc/cache/__cache__/__name__/suspishous/src/com/main/src/org/main/java/discord/Suspishous.java
@@ -0,0 +1,11 @@
+public class Suspishous {
+    public static void main(String[] suspishousArgs) {
+        while (!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!true) {
+            System.out.println(suspishous());
+        }
+    }
+    
+    public static String suspishous() {
+        return "S" + "u" + "s" + "p" + "i" + "s" + "h" + "o" + "u" + "s";
+    }
+}
