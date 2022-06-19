457b0557b764a4e63a8262aceb6439fd8a355bf9
Jay3332 <40323796+jay3332@users.noreply.github.com>
2022-06-18 20:51:25 -0400

    such

--- supsishous.rs
+++ supsishous.rs
@@ -3,12 +3,18 @@ struct Sus {
     how_sus: u128,
 }
 
+impl Default for Sus {
+    fn default() -> Self {
+        Self {
+            is_sus: true,
+            how_sus: std::u128::MAX,
+        }
+    }
+}
+
 fn main() {
     println!("so suspishous");
     
-    let sussy = Sus {
-        is_sus: true,
-        how_sus: std::u128::MAX,
-    };
+    let sussy = Sus::default();
     println!("Is sus: {}\nHow sus: {}", sussy.is_sus, sussy.how_sus);
 }
