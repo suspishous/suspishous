2904a2bec0da97486a33cccd5b8c8b5a653dd254
Jay3332 <40323796+jay3332@users.noreply.github.com>
2022-06-19 14:00:01 -0400

    upgrades

--- supsishous.rs
+++ supsishous.rs
@@ -1,4 +1,14 @@
+#![feature(prelude_import)]
+#![feature(fmt_internals)]
 #![no_main]
+
+#[prelude_import]
+use ::std::prelude::rust_2021::*;
+
+#[macro_use]
+extern crate std;
+#[macro_use]
+extern crate alloc;
 
 extern crate libc;
 
@@ -8,41 +18,42 @@ use ::std::rc::Rc;
 use ::std::sync::Arc;
 
 struct Sus {
-    is_sus: bool,
-    how_sus: u128,
-    is_very_sus: Rc<RefCell<Cell<Arc<Box<bool>>>>>,
-    amog: *const bool,
+    is_sus: ::core::primitive::bool,
+    how_sus: ::core::primitive::u128,
+    is_very_sus: Rc<RefCell<Cell<Arc<Box<::core::primitive::bool>>>>>,
+    amog: *const ::core::primitive::bool,
 }
 
 impl Default for Sus {
     fn default() -> Self {
         Self {
-            is_sus: true,
+            is_sus: 1 == 1,
             how_sus: ::std::u128::MAX,
-            is_very_sus: Rc::new(RefCell::new(Cell::new(Arc::new(Box::new(true))))),
-            amog: vec![true; 128].as_ptr(),
+            is_very_sus: ::std::rc::Rc::new(RefCell::new(Cell::new(Arc::new(Box::new(true))))),
+            amog: ::alloc::vec::from_elem(true, 128).as_ptr(),
         }
     }
 }
 
 #[no_mangle]
-pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+pub extern "C" fn main(_argc: ::core::primitive::isize,
+    _argv: *const *const ::core::primitive::u8) -> ::core::primitive::isize {
     let sussy = Sus::default();
-
-    const SUSPISHOUS: &str = "suspishous?!?!?!?!?!?!!!!\n\0";
-    unsafe {
-        libc::printf(SUSPISHOUS.as_ptr() as *const _);
-    }
-
-    let sus = format!("Is sus: {}\nHow sus: {}\n\0", sussy.is_sus, sussy.how_sus);
-    unsafe {
-        libc::printf(sus.as_str().as_ptr() as *const _);
-    }
-
+    const SUSPISHOUS: &'static ::core::primitive::str =
+        "suspishous?!?!?!?!?!?!!!!\n\0";
+        
+    unsafe { libc::printf(SUSPISHOUS.as_ptr() as *const _); }
+    let sus =
+        {
+            let res =
+                ::alloc::fmt::format(::core::fmt::Arguments::new_v1(&["Is sus: ",
+                                    "\nHow sus: ", "\n\u{0}"],
+                        &[::core::fmt::ArgumentV1::new_display(&sussy.is_sus),
+                                    ::core::fmt::ArgumentV1::new_display(&sussy.how_sus)]));
+            res
+        };
+    unsafe { libc::printf(sus.as_str().as_ptr() as *const _); }
     drop(sussy.is_very_sus);
-
-    unsafe {
-        drop(*sussy.amog);
-    }
+    unsafe { drop(*sussy.amog); }
     0
 }
